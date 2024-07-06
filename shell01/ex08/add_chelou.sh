ba8906ad9525a1653086993986ec4ff54b73314f
Javier Lopez <122676386+javilopercoder@users.noreply.github.com>
2024-07-06 12:19:31 +0200

    Add files via upload
    Subo los 6, 7 y 8 de shell01

---
+++ shell01/ex08/add_chelou.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo $FT_NBR1 = \'\?\"\\\"\'\\ + $FT_NBR2=rcrdmddd | sed "s/\'/0/g" | tr 
+'\\\"\?\!' 1234 | tr 'mrdoc' 01234 | xargs echo 'obase=13; ibase=5;' | bc 
+| tr 0123456789ABC 'gtaio luSnemf'
+
+# 1 Concatena las variables
+# 2 Reemplaza comillas simples por 0
+# 3 Traduce los caracteres especiales según la tabla 1234
+# 4 Ahora los caracteres según la tabla 01234
+# 5 Prepara el comando para el siguiente
+# 6 Realiza conversión numérica según lo especificado
+# 7 Lo traduce según la tabla gtaio luSnemf
