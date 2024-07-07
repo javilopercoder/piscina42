41052aea9769e8aa2409f3986c59d7b48f2f0411
Javier Lopez <122676386+javilopercoder@users.noreply.github.com>
2024-07-07 18:07:39 +0200

    Add files via upload
    Rush00

---
+++ Rush00/ex00/rush01.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: machirin <machirin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 15:19:05 by slargo-b          #+#    #+#             */
+/*   Updated: 2024/07/07 15:57:42 by machirin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	ft_print(int len, char begin, char midle, char end)
+{
+	int	letter_count;
+
+	letter_count = 1;
+	while (letter_count <= len)
+	{
+		if (letter_count == 1)
+			ft_putchar(begin);
+		else if (letter_count == len)
+			ft_putchar(end);
+		else
+			ft_putchar(midle);
+		letter_count++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	line_count;
+
+	line_count = 1;
+	if (x >= 1 && y >= 1)
+	{
+		while (line_count <= y)
+		{
+			if (line_count == 1)
+				ft_print(x, '/', '*', '\\');
+			else if (line_count == y)
+				ft_print(x, '\\', '*', '/');
+			else
+				ft_print(x, '*', ' ', '*');
+			line_count++;
+		}
+	}
+}
