02e476517af76cf9c0712d84d4329e73f1a58bb0
Javier Lopez <122676386+javilopercoder@users.noreply.github.com>
2024-07-10 22:16:33 +0200

    Add files via upload

---
+++ ex04/ft_strstr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalopez- <jalopez-@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 22:01:36 by jalopez-          #+#    #+#             */
+/*   Updated: 2024/07/10 22:15:22 by jalopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (to_find[j] == '\0')
+		return (str);
+	while (str[i] != '\0')
+	{
+		while (str[i + j] == to_find[j])
+			j++;
+		if (to_find[j] == '\0')
+			return (str + i);
+		i++;
+		j = 0;
+	}
+	return (0);
+}
+
+/*int	main()
+{
+	char str[] = "Me llamo Javier y en git me llamaba Jairo";
+	char to_find[] = "git";
+	printf("%s", ft_strstr(str, to_find));
+}*/
