f9e58cccb10c9b7c75e84ce6123b3fc617e69b64
Javier Lopez <122676386+javilopercoder@users.noreply.github.com>
2024-07-14 13:18:36 +0200

    Add files via upload

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalopez- <jalopez-@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 12:34:23 by jalopez-          #+#    #+#             */
+/*   Updated: 2024/07/14 12:50:20 by jalopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	i = 0;
+	while (i < (size / 2))
+	{
+		swap = tab [i];
+		tab[i] = tab[size - 1 - i];
+		tab [size - 1 - i] = swap;
+		i++;
+	}
+}
+
+/*int	main()
+{
+	int tab[5] = {0, 1, 2, 3, 4};
+	int size = 5;
+
+	ft_rev_int_tab(tab, size);
+	printf("%d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4]);
+	return (0);
+}*/
---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalopez- <jalopez-@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 12:54:41 by jalopez-          #+#    #+#             */
+/*   Updated: 2024/07/14 13:03:05 by jalopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	while (size >= 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if(tab[i] > tab[i + 1]);
+			{
+				tab[i] = tab[i + 1];
+				tab[i + 1] = swap;
+			}
+			i++;
+		}
+		size--;
+	}
+}
+
+int	main()
+{
+	int tab[5] = {9, 4, 3, 7, 1};
+	int size = 5;
+
+	ft_sort_int_tab(tab, size);
+	printf("%d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4]);
+	return(0);
+}
