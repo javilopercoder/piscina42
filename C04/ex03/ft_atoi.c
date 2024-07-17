d9ece5bd46f62e89429d7283c04a7ea3017c65f0
Javier Lopez <122676386+javilopercoder@users.noreply.github.com>
2024-07-17 16:05:38 +0200

    Add files via upload

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,9 +6,12 @@
 /*   By: jalopez- <jalopez-@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 20:56:58 by jalopez-          #+#    #+#             */
-/*   Updated: 2024/07/16 22:09:56 by jalopez-         ###   ########.fr       */
+/*   Updated: 2024/07/16 22:44:05 by jalopez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
 
 int	ft_atoi(char *str)
 {
@@ -18,20 +21,39 @@ int	ft_atoi(char *str)
 
 	i = 0;
 	while (str[i] != '\0' && (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
-			|| str[i] == '\r' || str[i] == '\t' str[i] == '\v'))
+			|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v'))
 		i++;
 	sign = 1;
-	while (str[i] != '\0' && (str[i]  == '-' || str[i] == '+'))
+	while (str[i] != '\0' && (str[i] == '-' || str[i] == '+'))
 	{
-		if  (str[i] == '-') 
+		if (str[i] == '-')
 			sign *= -1;
 		i++;
 	}
 	nb = 0;
-        while (str[i] != '\0' && str[i]  >= '0' && str[i] <= '9')
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
 	{
-		nb = nb *10 + str[i] - '0';
+		nb = nb * 10 + str[i] - '0';
 		i++;
 	}
 	return (nb * sign);
+}
+
+int	main(void)
+{
+	char	str1[12];
+	char	str2[12];
+	char	str3[12];
+	char	str4[12];
+	char	str5[12];
+
+	strcpy (str1, "	-123");
+	strcpy (str2, "456";
+	strcpy (str3[] = "	+92";
+	strcpy (str4, "	42abc42";
+	strcpy (str5, "abc";
+	printf ("String: '%s' -> Integer: %d\n", str1, ft_atoi(str1));
+	printf ("String: '%s' -> Integer: %d\n", str2, ft_atoi(str2));
+	printf ("String: '%s' -> Integer: %d\n", str5, ft_atoi(str5));
+	return (0);
 }
---
+++ Shell01/ex07/r_dwssap.sh
@@ -0,0 +1,12 @@
+cat /etc/passwd | sed '/^#/d' | awk 'NR % 2 == 0 {print $0}' | awk -F ":" '{  print $1 }' | rev | sort -fnr | awk -v FT_LINE1="$FT_LINE1" -v FT_LINE2="$FT_LINE2"  'NR >= FT_LINE1 && NR <= FT_LINE2 { print $0 }' | tr '\n' ' '  | sed 's+ +, +g' | sed 's+, $+.+g' | tr -d '\n'
+# 1 Lee el archivo passwd
+# 2 Elimina las líneas que comienzan con #
+# 3 Imprime las líneas pares
+# 4 Extrae el primer campo
+# 5 Invierte
+# 6 Ordena de manera numérica ignorango el caso (-f), en orden inverso
+# 7 Imprime las líneas en el rango especificado por las variables (han de estar definidas)
+# 8 Cambia los saltos de línea por espacios
+# 9 Ahora los que eran espacios por comas + espacio
+# 10 Reemplaza la última coma por un punto
+# 11 Quita los saltos de línea restantes
