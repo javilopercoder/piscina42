95a57b758fe23cbe98edd59e4a863dfdbb049b27
Javier Lopez <122676386+javilopercoder@users.noreply.github.com>
2024-07-18 17:27:34 +0200

    Add files via upload

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalopez- <jalopez-@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 17:13:21 by jalopez-          #+#    #+#             */
+/*   Updated: 2024/07/18 17:24:01 by jalopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+
+	i = 0;
+	while (argv[0][i] != '\0' && argc)
+	{
+		ft_putchar(argv[0][i]);
+		++i;
+	}
+	ft_putchar ('\n');
+	return (0);
+}
