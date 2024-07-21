508194dee73f6d43e007d0ba61e15be40c55946a
Javier Lopez <122676386+javilopercoder@users.noreply.github.com>
2024-07-21 03:25:16 +0200

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalopez- <jalopez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 01:44:52 by jalopez-          #+#    #+#             */
+/*   Updated: 2024/07/21 03:20:42 by jalopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+void	ft_swap(char **a, char **b)
+{
+	char	*tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void	ft_sort_str_tab(char **tab, int size)
+{
+	int		i;
+
+	while (size > 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (ft_strcmp(tab[i], tab[i + 1]) > 0)
+			{
+				ft_swap(&tab[i], &tab[i + 1]);
+			}
+			i++;
+		}
+		size--;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	ft_sort_str_tab(argv +1, argc -1);
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putstr("\n");
+		i++;
+	}
+	return (0);
+}
