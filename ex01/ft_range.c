112347e9361d4893abf0dc7b90606ded88b1c192
Javier Lopez <122676386+javilopercoder@users.noreply.github.com>
2024-07-21 16:38:50 +0200

    Add files via upload

---
+++ ex01/ft_range.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalopez- <jalopez-@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 06:58:09 by jalopez-          #+#    #+#             */
+/*   Updated: 2024/07/21 07:19:12 by jalopez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	*ft_range(int min, int max)
+{
+	int	range;
+	int	i;
+	int	*buffer;
+
+	if (min >= max)
+		return (0);
+	range = max - min;
+	buffer = malloc(range * sizeof(int));
+	if (!buffer)
+		return (0);
+	i = 0;
+	while (i < range)
+	{
+		buffer[i] = min + i;
+		i++;
+	}
+	return (buffer);
+}
+
+/*int	main(void)
+{
+	int	min;
+	int	max;
+	int	*tab;
+	int	i;
+	int	size;
+
+	i = 0;
+	min = 5;
+	max = 10;
+	size = max - min;
+	tab = ft_range(min, max);
+	while (i < size)
+	{
+		printf("%d, ", tab[i]);
+		i++;
+	}
+	free (tab);
+	return (0);
+}*/
